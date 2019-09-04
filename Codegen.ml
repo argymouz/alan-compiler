5430a0fb8a035989c5987a2860eb63df4817cb16
amouzakis17@hotmail.com <amouzakis17@hotmail.com>
2019-09-04 22:39:40 +0300

    Corrections + Comments

--- Codegen.ml
+++ Codegen.ml
@@ -63,22 +63,22 @@ and codegen tree =
 		raise (Failure "CODEGEN CALLED FOR PROGRAM NODE\n")
 	| Func_def_t(name, par_l, ret_type, loc_def_l, comp_body, _) ->
 	(
-		let par_arr = opt_list_to_array par_l in
-		let par_typ_arr1 = Array.map extract_param_type par_arr in
-		let par_typ_arr = add_frame_ptr name par_typ_arr1 in
-		let ft = function_type (ret_ll_typ ret_type) par_typ_arr in
-		let f = declare_function name ft the_module in
-		let par_names1 = Array.map extract_param_name par_arr in
-		let par_names = Array.append [| "fr_ptr" |] par_names1 in
+		let par_arr = opt_list_to_array par_l in (* convert parameter list to array *)
+		let par_typ_arr1 = Array.map extract_param_type par_arr in (* extract parameter types *)
+		let par_typ_arr = add_frame_ptr_typ name par_typ_arr1 in (* for every function except main, add an extra element that will work as an access link *)
+		let ft = function_type (ret_ll_typ ret_type) par_typ_arr in (* now we know the function's type *)
+		let f = declare_function name ft the_module in (* and we can declare it *)
+		let par_names1 = Array.map extract_param_name par_arr in (* extract parameter names *)
+		let par_names = add_frame_ptr_name name par_names1 in (* add the extra element's name, if it exists *)
 		let param_arr = params f in (
 			Array.iteri (fun i a ->
-				let n = Array.get par_names i in
+				let n = Array.get par_names i in (* set the parameters' names to the llvm code *)
 				set_value_name n a;
 			) param_arr;
-			let type_lst = extract_loc_var_def_type loc_def_l [] in
-			let type_arr = opt_list_to_array type_lst in
-			let frame_typ_arr = Array.append par_typ_arr type_arr in
-			let frame_type = struct_type context frame_typ_arr in
+			let type_lst = extract_loc_var_def_type loc_def_l [] in (* extract the types of local variables *)
+			let type_arr = opt_list_to_array type_lst in (* convert list to array *)
+			let frame_typ_arr = Array.append par_typ_arr type_arr in (* create the array with the type of the frame *)
+			let frame_type = struct_type context frame_typ_arr in (* create the frame *)
 			let frame_type_str = string_of_lltype frame_type in
 			(
 				(
@@ -89,11 +89,11 @@ and codegen tree =
 						ignore(Stack.push curr_top type_stack);
                 			)
 				);
-				let llvm_fr_str = build_global_string frame_type_str "" builder in
-				let bb = append_block context "entry" f in
-                                let fr = create_entry_block_alloca f (String.concat "." [name; "fr"]) frame_type in
+				(*let llvm_fr_str = build_global_string frame_type_str "" builder in*)
+				let bb = append_block context "entry" f in (* now create a basic block *)
+                                let fr = create_entry_block_alloca f (String.concat "." [name; "fr"]) frame_type in (* allocate the stack frame *)
 				(
-					init_fr fr param_arr 0 (Array.length param_arr);
+					init_fr fr param_arr 0 (Array.length param_arr); (* initialize the frame, see the def of the function below *)
 					ignore(List.map codegen_loc_fun loc_def_l);
 					ignore(codegen_body comp_body fr);
 					ignore(Stack.pop type_stack);
@@ -104,6 +104,7 @@ and codegen tree =
 			)
 		)
 	)
+        | _ -> raise (Failure "This should not happen!\n")
 
 and codegen_body body fr =
 	match body with
@@ -122,7 +123,7 @@ and codegen_body body fr =
 		let callee = deopt_lookup_function name in
 		let args_array = opt_list_to_array args_l in
 		let args = Array.mapi (codegen_call callee fr) args_array in
-		let search_frames = deopt_lookup_function name in
+		let search_frames = deopt_lookup_function "search_frames" in
 		let parent_frame_ptr = build_call search_frames [| fr; const_int myint depth |] "" builder in
 		build_call callee (Array.append [| parent_frame_ptr |] args) "" builder
         | If_t(cond, then_, dtyp) ->
@@ -387,10 +388,17 @@ and codegen_search_frames () =
 		const_null myint
 	)
 
-and add_frame_ptr name arr =
+and add_frame_ptr_name name arr =
+        match name with
+        | "main" -> arr
+        | _ -> Array.append [| "fr_ptr" |] arr
+
+and add_frame_ptr_typ name arr =
 	match name with
 	| "main" -> arr
 	| _ -> Array.append [| i64_type context |] arr
+
+(* the part of the frame that has to be initialized is the one corresponding to the parameters, not the local variables and the functions *)
 
 and init_fr fr param_arr idx n =
 	match idx with
@@ -456,6 +464,8 @@ and ret_ll_typ a =
 	| Proc -> myvoid
 	| _ -> raise (Failure "Invalid ret type!\n")
 
+(* this function and the next operate on arrays*)
+
 and extract_param_name a =
 	match a with
 	| Fpar_def_t(name, dtyp, _) -> name
@@ -480,6 +490,8 @@ and extract_param_type a =
 		| ByteArr ->  mybyteref
 	)
 	| _ -> raise (Failure "Improper extract_param_type usage")
+
+(* this function operates on lists *)
 
 and extract_loc_var_def_type lst type_lst = 
 	match lst with
