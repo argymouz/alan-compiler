27a4c84684529304c80e39d975ded8a5bcf95bd3
nim1821 <el14008@central.ntua.com>
2019-09-23 20:27:02 +0300

    main and makefile

--- Main.ml
+++ Main.ml
@@ -9,6 +9,71 @@ open Types
 open Symbol
 open Error
 open Codegen
+open Filename
+
+let optimization = ref false
+let intermediate = ref false
+let final = ref false
+let main = 
+begin
+let speclist = [("-O", Arg.Set optimization, "Enables optimization");
+("-i", Arg.Set intermediate, "Produces intermediate code from stdin");
+("-f", Arg.Set final, "Produces final code from stdin");] 
+in let usage_msg="A compiler for the Alan Language"
+in let compiler_func input_file =
+begin
+	let cin = open_in input_file in
+        let basefile = basename input_file in
+        let dir = dirname input_file in
+        let truename = remove_extension basefile in
+        let immfile = concat dir (truename^".imm") in
+	let asmfile = concat dir (truename^".asm") in	
+        let exefile = concat dir truename in
+        let lexbuf = Lexing.from_channel cin in
+	try
+   		let ast = Parser.program Lexer.lexer lexbuf in
+    		let t_ast = typing ast in 
+                let optflag = match !optimization with false -> "no" | true -> "yes" in
+		let the_module = Codegen.top_codegen t_ast optflag in
+                print_module immfile the_module;
+                (        
+                match (Sys.command("/usr/lib/llvm-6.0/bin/llc "^immfile^" -o "^asmfile)) with
+                0 -> exit (Sys.command("clang "^asmfile^" lib.a -o "^exefile))
+                | a -> exit a
+                )
+        with Parser.Error ->
+    		Error.error "%a syntax error\n" Error.print_position (Error.position_point lexbuf.Lexing.lex_curr_p);
+        exit 1
+end
+in Arg.parse speclist compiler_func usage_msg;
+(match (!intermediate)||(!final) with
+false -> ()
+|true -> (        
+	let lexbuf = Lexing.from_channel stdin in
+	try
+   		let ast = Parser.program Lexer.lexer lexbuf in
+    		let t_ast = typing ast in 
+                let optflag = match !optimization with false -> "no" | true -> "yes" in
+		let the_module = Codegen.top_codegen t_ast optflag in
+                print_module "output.ll" the_module;
+		(
+		match(!final) with
+        	false -> exit (Sys.command("cat output.ll"))
+        	| true -> (        
+                match (Sys.command("/usr/lib/llvm-6.0/bin/llc output.ll -o output.s")) with
+                0 -> exit (Sys.command("cat output.s"))
+                | a -> exit a
+                )
+        	)
+        with Parser.Error ->
+    		Error.error "%a syntax error\n" Error.print_position (Error.position_point lexbuf.Lexing.lex_curr_p);
+	
+        )
+        
+)
+end
+(*
+
 
 let main =
  let cin = stdin in
@@ -22,3 +87,5 @@ let main =
     Error.error "%a syntax error\n" Error.print_position (Error.position_point lexbuf.Lexing.lex_curr_p);
     Printf.printf "Compilation FAILED :(\n";
     exit 1
+
+*)
--- Makefile
+++ Makefile
@@ -1,17 +1,10 @@
-.PHONY: clean distclean pack count
+.PHONY: clean distclean 
 
-# OS type: Linux/Win DJGPP
-ifdef OS
-   EXE=.exe
-else
-   EXE=
-endif
-
-EXEFILE=alan$(EXE)
+EXEFILE=alan
 MLFILES= Hashcons.ml Identifier.ml Error.ml Types.ml Symbol.ml \
-  Lexer.ml Ast.ml Parser.ml ParserError.ml Semantic.ml Codegen.ml Main.ml
+   Ast.ml Parser.ml ParserError.ml Lexer.ml Semantic.ml Codegen.ml Main.ml
 MLIFILES=Hashcons.mli Identifier.mli Error.mli Types.mli Symbol.mli \
-  Parser.mli Ast.mli ParserError.mli Lexer.mli Semantic.mli Codegen.mli
+   Ast.mli Parser.mli ParserError.mli Lexer.mli Semantic.mli Codegen.mli
 CMOFILES=$(patsubst %.ml,%.cmo,$(MLFILES))
 CMIFILES=$(patsubst %.ml,%.cmi,$(MLFILES))
 OBJFILES=$(patsubst %.ml,%.o,$(MLFILES))
@@ -25,9 +18,12 @@ OCAMLC_FLAGS=
 OCAMLC=ocamlc $(OCAMLC_FLAGS)
 OCAMLDEP=ocamldep
 PACKAGE= -package llvm -package llvm.analysis -package llvm.target -package llvm.scalar_opts -package llvm.all_backends -package unix
-default: alan$(EXE)
-cmi: $(CMIFILES)
-all: $(EXEFILE) $(CMIFILES) lib.a
+
+all: $(CMIFILES) $(EXEFILE) lib.a
+
+$(EXEFILE): Parser.mli Lexer.ml $(CMOFILES)
+	ocamlfind $(OCAMLC) $(PACKAGE) -linkpkg -o $@ $(CMOFILES) 
+
 extend.cmo: extend.ml
 	$(OCAMLC) -pp "camlp5o pa_extend.cmo q_MLast.cmo" -I `camlp5 -where` -c $<
 
@@ -40,28 +36,18 @@ lib.a:
 %.cmo %.cmi: %.ml extend.cmo
 	ocamlfind $(OCAMLC) $(CAMLP5_FLAGS) $(PACKAGE) -c $<
 
-.PHONY: all clean count depend
-
-$(EXEFILE): Parser.mli Lexer.ml $(CMOFILES)
-	ocamlfind $(OCAMLC) $(PACKAGE) -linkpkg -o $@ $(CMOFILES) 
-
 Parser.ml Parser.mli: Parser.mly
 	menhir -v Parser.mly
 
 Lexer.ml: Lexer.mll
 	ocamllex Lexer.mll
 
--include .depend
+-include depend
 
 depend: $(MLFILES) $(MLIFILES) extend.cmo
 	$(OCAMLDEP) $(CAMLP5_FLAGS) $(INCLUDES) \
           $(filter-out extend.cmo,$^) > .depend
 
-depend-alan: Hashcons.ml Identifier.ml Error.ml Types.ml Symbol.ml \
-                 Main.ml Hashcons.mli Identifier.mli Error.mli Types.mli \
-                 Symbol.mli Codegen.mli extend.cmo
-	$(OCAMLDEP) $(CAMLP5_FLAGS) $(INCLUDES) \
-          $(filter-out extend.cmo,$^) > .depend
 
 clean:
 	$(RM) $(CMOFILES) $(CMIFILES) $(OBJFILES) $(EXEFILES) \
